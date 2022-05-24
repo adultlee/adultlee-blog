6bb5041eeb654873fcefd75bdd5bc950c53d4f9f
adultlee <seoungin1228@gmail.com>
2022-05-24 16:00:58 +0900

    feat: 마우스 이펙트

--- src/layout/index.js
+++ src/layout/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { useStaticQuery, graphql } from "gatsby";
-import MouseParticles from "react-mouse-particles";
-
+// import MouseParticles from "../components/MouseEffect";
 import PageHeader from "../components/page-header";
 import PageFooter from "../components/page-footer";
 import ThemeSwitch from "../components/theme-switch";
 import "./style.scss";
+
+const CSOnlyMouseParticles = React.lazy(() =>
+  import("../components/MouseEffect")
+);
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -25,16 +28,21 @@ const Layout = ({ children }) => {
   `);
   const { title, author } = data.site.siteMetadata;
 
+  const isSSR = typeof window === "undefined";
   return (
     <div className="page-wrapper">
       <PageHeader siteTitle={title || `Title`} />
       <main className="page-content">{children}</main>
-      <MouseParticles
-        g={1}
-        color="random"
-        cull="MuiSvgIcon-root,MuiButton-root"
-        level={6}
-      />
+      {!isSSR && (
+        <React.Suspense fallback={<div />}>
+          <CSOnlyMouseParticles
+            g={1}
+            color="random"
+            cull="MuiSvgIcon-root,MuiButton-root"
+            level={6}
+          />
+        </React.Suspense>
+      )}
       <PageFooter
         author={author.name || `Author`}
         githubUrl={author.social?.github || `https://www.github.com`}
